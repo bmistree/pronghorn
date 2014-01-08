218b182a4d14c4242cfd97d41f8f1e2b11c7216d
behram mistree <bmistree@gmail.com>
2014-01-07 19:09:38 -0800

    Fixed bug where may not have been indexing into correct value in routing table when pushing change.

--- src/pronghorn/src/FloodlightRoutingTableToHardware.java
+++ src/pronghorn/src/FloodlightRoutingTableToHardware.java
@@ -5,6 +5,9 @@ import RalphDataWrappers.ListTypeDataWrapper;
 import pronghorn.RTable._InternalRoutingTableEntry;
 import ralph.RalphObject;
 import RalphExceptions.BackoutException;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.HashMap;
 
 /**
    Subclass this object to override behavior of internal list when
@@ -41,42 +44,108 @@ public class FloodlightRoutingTableToHardware extends RoutingTableToHardware
         int prev = ++unique_entry_name_generator;
         return Integer.toString(prev);
     }
-    
-    @Override
-    public boolean apply_changes_to_hardware(
+
+    private ArrayList<RTableUpdate> produce_rtable_updates(
         ListTypeDataWrapper<
             _InternalRoutingTableEntry,_InternalRoutingTableEntry> dirty)
     {
-        // backup previous version of entry names in case we need to
-        // back out changes to hardware.
-        prev_entry_names = new ArrayList(entry_names);
-        
-        ArrayList<RTableUpdate> floodlight_updates = new ArrayList<RTableUpdate>();
+        /*
+          We have a list of operations that were performed on a list and the
+          final contents of the list.  We want to take these changes and
+          push the relevant deltas to hardware.
 
-        ArrayList<RalphObject<_InternalRoutingTableEntry,_InternalRoutingTableEntry>>
-            dirty_list = dirty.val;
+          The challenge is if we have some set of operations like:
+
+             add to index 0
+             add to index 1
+             remove from index 0
+
+          Then, if we just run through the list of operations sequentially,
+          when we get to the second operation (add to index 1), we do not
+          know where the object will be stored in the final array list and
+          therefore will not be able to read the changes that that operation
+          makes to the routing table.
+
+          Instead, we use a different algorithm:
+
+            1) Run through all changes to routing table and
+
+                a) update entry_names: a list that stores the routing table
+                   entry name associated with each index in the routing
+                   table list.  After 1, entry_names should be a list of
+                   names for routing table entries that would be stored on
+                   the switch after pushing the change.
+
+                b) Map each added tuple to its entry name.  For each add
+                   tuple, assign it a unique entry name and create a map
+                   from add tuples to entry names.
+
+             2) Run through the list of changes to the routing table again.
+                Each time notice an added tuple:
+
+                a) Check that the added tuple was not in the removed tuple.
+                   If it was, then we had added an entry and then removed it:
+                   do not need to push these changes to switch.
+
+                b) Create an update for each added tuple that was not
+                   removed.  First need to find the index of the associated
+                   change in the ralph list.  Can do this by using map to
+                   find add_tuple's associated unique name and then locating
+                   the index of this unique name in the entry_name list.
+
+             3) Add a remove update for each unique name that is still in
+                entries_to_remove --- a set containing the names of all entries
+                that were removed by this transaction (and not added by it also).
+         */
+        Set<String> entries_to_remove = new HashSet<String>();
+        HashMap<ListTypeDataWrapper.OpTuple,String> added_tuples_to_hidden_names =
+            new HashMap<ListTypeDataWrapper.OpTuple,String>();
+
+
+        // Step 1 from above
         for (ListTypeDataWrapper.OpTuple op_tuple : dirty.partner_change_log)
         {
             if (dirty.is_delete_key_tuple(op_tuple))
             {
                 int deleted_key = op_tuple.key;
+                // 1a from above
                 String removed_entry_name = entry_names.remove(deleted_key);
-                floodlight_updates.add(
-                    RTableUpdate.create_remove_update(removed_entry_name));
+                entries_to_remove.add(removed_entry_name);
             }
             else if (dirty.is_add_key_tuple(op_tuple))
             {
                 int index_added = op_tuple.key;
-                // HUGE FIXME
-                System.out.println(
-                    "\nFIXME: index_added may not map to final dirty value of array\n");
+                // 1a from above
+                String entry_name = new_unique_entry_name ();
+                entry_names.add(index_added,entry_name);
+                // 1b from above
+                added_tuples_to_hidden_names.put(op_tuple,entry_name);
+            }
+        }
 
-                // when we call get, we get the wrapped version of the
-                // object, not the object itself.
-                RalphObject ro = dirty_list.get(index_added);
-                
+        // Step 2 from above
+        ArrayList<RTableUpdate> floodlight_updates = new ArrayList<RTableUpdate>();
+        ArrayList<RalphObject<_InternalRoutingTableEntry,_InternalRoutingTableEntry>>
+            dirty_list = dirty.val;
+        for (ListTypeDataWrapper.OpTuple op_tuple : dirty.partner_change_log)
+        {
+            if (dirty.is_add_key_tuple(op_tuple))
+            {
+                String entry_name = added_tuples_to_hidden_names.get(op_tuple);
+
+                // Step 2a from above
+                // check if we added the entry and then removed it: do not
+                // need to push addition or removal to hardware.
+                if (entries_to_remove.remove(entry_name))
+                    continue;
+
+                // Step 2b from above
+                // entry was added and not deleted: get associated object
+                // and create an update for it
+                int added_entry_index = entry_names.indexOf(entry_name);
+
+                RalphObject ro = dirty_list.get(added_entry_index);
                 _InternalRoutingTableEntry added_entry = null;
-
                 try {
                     added_entry = (_InternalRoutingTableEntry)(ro.get_val(null));                    
                 } catch (BackoutException _ex) {
@@ -87,41 +156,38 @@ public class FloodlightRoutingTableToHardware extends RoutingTableToHardware
                     assert(false);
                 }
 
-                // warning assumption is that no other elements are touching
-                // this state and we can therefore read/write it.
-                String entry_name = new_unique_entry_name ();
-                entry_names.add(index_added,entry_name);
                 String src_ip = added_entry.src_ip.dirty_val.val;
                 String dst_ip = added_entry.dst_ip.dirty_val.val;
                 Double action = added_entry.action.dirty_val.val;
 
                 System.out.println(
                     "Warning: using a number for action instead of string.");
-                
+
                 RTableUpdate update_to_push =  RTableUpdate.create_insert_update(
                     entry_name, src_ip, dst_ip,action.toString());
 
                 floodlight_updates.add(update_to_push);
             }
-            else if (dirty.is_write_key(op_tuple))
-            {
-                // FIXME: not handling writes to keys: only handling
-                // additions and removals to start with.
-                System.out.println(
-                    "FIXME: still must define rtable changes for writes.");
-                assert(false);
-            }
-            // DEBUG
-            else
-            {
-                // FIXME: change from system.out.println
-                System.out.println("Unknown tuple type.");
-                assert(false);
-            }
-            // END DEBUG
         }
 
-        // force the shim to push the changes to swithces.
+        // Step 3 from above
+        for (String removed_entry_name : entries_to_remove)
+            floodlight_updates.add(
+                RTableUpdate.create_remove_update(removed_entry_name));
+        
+        return floodlight_updates;
+    }
+    
+    @Override
+    public boolean apply_changes_to_hardware(
+        ListTypeDataWrapper<
+            _InternalRoutingTableEntry,_InternalRoutingTableEntry> dirty)
+    {
+        // backup previous version of entry names in case we need to
+        // back out changes to hardware.
+        prev_entry_names = new ArrayList(entry_names);
+        ArrayList<RTableUpdate> floodlight_updates = produce_rtable_updates(dirty);
+        // request shim to push the changes to swithces.
         return shim.switch_rtable_updates(
             floodlight_switch_id,floodlight_updates);
     }
@@ -132,6 +198,7 @@ public class FloodlightRoutingTableToHardware extends RoutingTableToHardware
         to_undo)
     {
         entry_names = prev_entry_names;
-        System.out.println("FIXME: Still need undo changes made to routing table.");
+        System.out.println(
+            "FIXME: Still need undo changes made to routing table.");
     }
 }

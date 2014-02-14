4cf6ec37bbf3ff89f639daf3c11ded550e5f6617
behram mistree <bmistree@gmail.com>
2014-02-13 16:13:58 -0800

    Updated new floodlight interface to track switches' entering and leaving network.

--- src/experiments/single_host/src/SingleHostFloodlightShim.java
+++ src/experiments/single_host/src/SingleHostFloodlightShim.java
@@ -1,6 +1,8 @@
 package single_host;
 
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.Set;
+import java.util.HashSet;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import pronghorn.ShimInterface;
@@ -11,27 +13,64 @@ import net.floodlightcontroller.pronghornmodule.IPronghornService;
 import net.floodlightcontroller.core.Main;
 import net.floodlightcontroller.core.module.FloodlightModuleException;
 import net.floodlightcontroller.pronghornmodule.IPronghornService;
-
+import net.floodlightcontroller.pronghornmodule.ISwitchAddedRemovedListener;
 /**
    Serves as intermediate layer between Ralph and Floodlight
  */
-public class SingleHostFloodlightShim implements Runnable, ShimInterface
+public class SingleHostFloodlightShim
+    implements Runnable, ShimInterface, ISwitchAddedRemovedListener
 {
     private IPronghornService pronghorn_floodlight = null;
+
+    /**
+       Whenever we see that there was a new switch or we see that a
+       switch went down, we notify these handlers. 
+     */
+    private ReentrantLock handler_lock = new ReentrantLock();
+    private Set<SwitchStatusHandler> switch_status_handlers =
+        new HashSet<SwitchStatusHandler>();
+
+
     
     public SingleHostFloodlightShim(String settings_filename)
     {
-        Main.ProviderPronghornTuple ppt = null;
+
         try
         {
-            ppt = Main.get_controller(settings_filename);
+            final Main.ProviderPronghornTuple ppt =
+                Main.get_controller(settings_filename);
             pronghorn_floodlight = ppt.pronghorn;
+
+
+            Thread t = new Thread()
+            {
+                @Override
+                public void run()
+                {
+                    ppt.floodlight_provider.run();
+                }
+            };
+            t.start();
+
+            try
+            {
+                Thread.sleep(50);
+            }
+            catch (InterruptedException ex)
+            {
+                ex.printStackTrace();
+                assert(false);
+            }
+
+            pronghorn_floodlight.register_switch_changes_listener(this);
+
         }
         catch (FloodlightModuleException ex)
         {
             ex.printStackTrace();
             assert(false);
         }
+
     }
 
     @Override
@@ -39,14 +78,39 @@ public class SingleHostFloodlightShim implements Runnable, ShimInterface
     {
     }
     
+    /** ISwtichAddedRemovedListener */
+    @Override
+    public void switch_added(String unique_switch_id)
+    {
+        handler_lock.lock();
+        for (SwitchStatusHandler ssh : switch_status_handlers)
+            ssh.new_switch(this,unique_switch_id);
+        handler_lock.unlock();
+    }
+    @Override    
+    public void switch_removed(String unique_switch_id)
+    {
+        handler_lock.lock();
+        for (SwitchStatusHandler ssh : switch_status_handlers)
+            ssh.removed_switch(this,unique_switch_id);
+        handler_lock.unlock();
+    }
+    
+    
     /** ShimInterface methods */
     @Override
     public void subscribe_switch_status_handler(SwitchStatusHandler ssh)
     {
+        handler_lock.lock();
+        switch_status_handlers.add(ssh);
+        handler_lock.unlock();
     }
     @Override
     public void unsubscribe_switch_status_handler(SwitchStatusHandler ssh)
     {
+        handler_lock.lock();
+        switch_status_handlers.remove(ssh);
+        handler_lock.unlock();
     }
     
     @Override

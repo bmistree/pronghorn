65c4c1a70e81000fdf394aae7bd11f08520d8011
behram mistree <bmistree@gmail.com>
2014-03-20 12:47:21 -0700

    Added documentation for expected behavior of pronghorn switch guard and its interface to ralph runtime.  Still must actually implement this interface.

--- src/pronghorn/src/InternalPronghornSwitchGuard.java
+++ src/pronghorn/src/InternalPronghornSwitchGuard.java
@@ -22,6 +22,88 @@ import pronghorn.SwitchDeltaJava._InternalSwitchDelta;
 import pronghorn.FTable._InternalFlowTableEntry;
 import pronghorn.SwitchJava._InternalSwitch;
 
+/**
+   Interfaces to Ralph runtime:
+
+     hardware_first_phase_commit_hook
+       Returns a future.  This future can be cancelled by ralph
+       runtime, while hardware is still pushing changes.  In this
+       case, guaranteed to get a call to hardware_backout_hook.  That
+       call should block until all changes have been removed from
+       hardware.
+
+       It should be impossible to get another
+       hardware_first_phase_commit_hook while we are backing out.
+       Adding an assert to ensure.
+
+
+     hardware_complete_commit_hook
+       Changes that have been pushed can be released.  Only called
+       after hardware_first_phase_commit_hook.
+
+     hardware_backout_hook
+       Backout changes that may have been made to a hardware element.
+       Only return after barrier response that changes have completely
+       been removed from hardware.
+
+     hardware_first_phase_commit_speculative_hook
+       
+
+   Corresponding states:
+
+     CLEAN --- No outstanding changes on hardware.
+
+     PUSHING_CHANGES --- Transition into this state from a
+     hardwrae_first_phase_commit_hook. State means that we are in the
+     midst of pushing first phase commits to hardware and/or waiting
+     on barrier that they have completed.  If barrier times out or get
+     an error, transition into failed state.  Note: 1) If just got
+     errors on operations, could retry those operations with barriers.
+     Currently, do not and just go to failed.  2) Need to deal with
+     failed state, creating super priority that removes state.
+
+     STAGED_CHANGES --- Dirty state is on hardware and hardware has
+     acknowledged this with a barrier response and no intervening
+     error messages.  If commit succeeds (get
+     hardware_complete_commit_hook), go ahead and go back to clean.
+
+     REMOVING_CHANGES --- Enters when in STAGED_CHANGES state and
+     receive an event from hardware_backout_hook.  Can transition to
+     Failed if undos it issues error out or barrier times out.  (Same
+     FIXME-s as PUSHING_CHANGES state.)  Transitions to CLEAN if all
+     changes are removed.  Does not return until they are all removed.
+
+     FAILED --- Switch must be reset by an admin.
+
+
+
+   +--------------+        +---------------+
+   |              |        |               |
+   |              |        | PUSHING       |
+   |    CLEAN     | -----> |   CHANGES     |---------
+   |              |        |               |         |
+   +--------------+        +---------------+         |
+        ^     ^                        |             |
+        |     |                        |             |
+        |      ----------------        |             |
+        |                      |       v             |
+   +--------------+        +---------------+         |
+   |              |        |               |         |
+   |  REMOVING    | <----- | STAGED        |         |
+   |    CHANGES   |        |   CHANGES     |         |
+   |              |        |               |         |
+   +--------------+        +---------------+         |
+             |                                       |
+             |                                       |
+             v                                       |
+      +------------+                                 |
+      |            |                                 |
+      |  FAILED    | <--------------------------------
+      |            |
+      +------------+
+ */
+
+
 public class InternalPronghornSwitchGuard extends AtomicNumberVariable
 {
     public final String ralph_internal_switch_id;
@@ -30,11 +112,11 @@ public class InternalPronghornSwitchGuard extends AtomicNumberVariable
     private final FlowTableToHardware to_handle_pushing_changes;
     private final ExecutorService hardware_push_service;
     private final boolean should_speculate;
-    
+
     private List<RalphObject<_InternalFlowTableDelta,_InternalFlowTableDelta>>
         dirty_on_hardware = null;
-    
-    
+
+
     /**
        @param to_handle_pushing_changes --- Can be null, in which
        case, will simulate pushing changes to hardware.  (Ie.,
@@ -64,13 +146,13 @@ public class InternalPronghornSwitchGuard extends AtomicNumberVariable
         // these accesses are safe, because we assume the invariant
         // that will only receive changes on PronghornSwitchGuard
         // if no other event is writing to them.
-        
+
         // grabbing ft_deltas to actually get changes made to hardware.
         AtomicListVariable<_InternalFlowTableDelta,_InternalFlowTableDelta>
             ft_deltas_list = switch_delta.ft_deltas;
         AtomicInternalList<_InternalFlowTableDelta,_InternalFlowTableDelta>
             internal_ft_deltas_list = null;
-        
+
         if (ft_deltas_list.dirty_val != null)
             internal_ft_deltas_list = ft_deltas_list.dirty_val.val;
         else
@@ -102,7 +184,8 @@ public class InternalPronghornSwitchGuard extends AtomicNumberVariable
 
 
     /**
-       Called from within lock.
+       Called from within lock.  Only gets called on non-derivative
+       objects.
 
        @returns --- Can be null, eg., if the object is not backed by
        hardware.  Otherwise, call to get on future returns true if if
@@ -114,8 +197,8 @@ public class InternalPronghornSwitchGuard extends AtomicNumberVariable
     {
         // FIXME: Check next paragraphs now that updated method to be
         // called from within lock.
-        
-        
+
+
         // do not need to take locks here because know that this
         // method will only be called from AtomicActiveEvent
         // during first_phase_commit. Because AtomicActiveEvent is
@@ -127,11 +210,11 @@ public class InternalPronghornSwitchGuard extends AtomicNumberVariable
         AtomicInternalList<_InternalFlowTableEntry,_InternalFlowTableEntry>
             internal_ft_list = get_internal_ft_list();
         AtomicInternalList<_InternalFlowTableDelta,_InternalFlowTableDelta>
-            internal_ft_deltas_list = get_internal_ft_deltas_list(); 
+            internal_ft_deltas_list = get_internal_ft_deltas_list();
 
         ArrayList<RalphObject<_InternalFlowTableDelta,_InternalFlowTableDelta>>
             to_push = null;
-        
+
         if (should_speculate)
         {
             internal_ft_list.speculate(active_event,null);
@@ -139,12 +222,12 @@ public class InternalPronghornSwitchGuard extends AtomicNumberVariable
                 active_event,
                 // so that resets delta list
                 new ArrayList<RalphObject<_InternalFlowTableDelta,_InternalFlowTableDelta>>());
-            
+
             speculate(active_event,null);
         }
         else
         {
-            if ((write_lock_holder != null) && 
+            if ((write_lock_holder != null) &&
                 (active_event.uuid.equals(write_lock_holder.event.uuid)))
             {
                 to_push = internal_ft_deltas_list.dirty_val.val;
@@ -155,13 +238,13 @@ public class InternalPronghornSwitchGuard extends AtomicNumberVariable
         // FIXME: What if this is on top of a speculated value.
         // Doesn't that mean that write_lock_holder might be
         // incorrect/pointing to incorrect value?
-        if ((write_lock_holder != null) && 
+        if ((write_lock_holder != null) &&
             (active_event.uuid.equals(write_lock_holder.event.uuid)))
         {
             // check if we're just supposed to be simulating changes.
             if (to_handle_pushing_changes == null)
                 return null;
-            
+
             dirty_on_hardware = to_push;
 
             WrapApplyToHardware to_apply_to_hardware =
@@ -175,7 +258,7 @@ public class InternalPronghornSwitchGuard extends AtomicNumberVariable
             // guard/flowtabletohardware.  Need to update to handle
             // case where can get backed out before all changes have
             // been pushed to hardware.
-            
+
             // return to_apply_to_hardware.to_notify_when_complete;
             assert(false);
          }
@@ -185,7 +268,7 @@ public class InternalPronghornSwitchGuard extends AtomicNumberVariable
         // do not need to ensure that hardware is up (for now).  May
         // want to add read checks as well.
         return ALWAYS_TRUE_FUTURE;
-    }                
+    }
 
     @Override
     protected void hardware_complete_commit_hook(ActiveEvent active_event)
@@ -240,7 +323,7 @@ public class InternalPronghornSwitchGuard extends AtomicNumberVariable
 
         return true;
     }
-    
+
     // FIXME: must override acquire and release locks in case switch
     // fails.
 }

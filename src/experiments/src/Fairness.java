acad1d0c79e1a619824c7b6332b368be91a65b48
behram mistree <bmistree@gmail.com>
2014-04-16 12:50:33 -0700

    Refactored fairness experiment.  Now have a separate utility library for fairness experiment.  Goal is to add another single controller fairness experiment that can call into this library, as the current fairness experiment does.

--- src/experiments/src/Fairness.java
+++ src/experiments/src/Fairness.java
@@ -1,39 +1,28 @@
 package experiments;
 
-import pronghorn.SingleInstanceFloodlightShim;
-import pronghorn.SingleInstanceSwitchStatusHandler;
-import pronghorn.InstanceJava.Instance;
-import experiments.GetNumberSwitchesJava.GetNumberSwitches;
-import experiments.MultiControllerOffOnJava.MultiControllerOffOn;
-import experiments.IOffOnApplicationJava.IOffOnApplication;
+import java.io.IOException;
+import java.lang.Thread;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import RalphConnObj.SingleSideConnection;
 import ralph.RalphGlobals;
-import ralph.NonAtomicInternalList;
 import ralph.Endpoint;
 import ralph.EndpointConstructorObj;
-import pronghorn.FloodlightFlowTableToHardware;
-import java.lang.Thread;
-import java.util.ArrayList;
-import pronghorn.FTableUpdate;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
 import ralph.Ralph;
 import ralph.RalphGlobals;
 import ralph.BoostedManager.DeadlockAvoidanceAlgorithm;
 
-import java.util.HashSet;
-import java.util.Set;
-import experiments.Util.HostPortPair;
-import experiments.Util;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.io.IOException;
+import pronghorn.FloodlightFlowTableToHardware;
+import pronghorn.FTableUpdate;
+import pronghorn.SingleInstanceFloodlightShim;
+import pronghorn.SingleInstanceSwitchStatusHandler;
+import pronghorn.InstanceJava.Instance;
+
+import experiments.GetNumberSwitchesJava.GetNumberSwitches;
+import experiments.MultiControllerOffOnJava.MultiControllerOffOn;
+import experiments.Util;
+import experiments.FairnessUtil;
 
 /**
    Experiment, start a number of transactions on one controller, then,
@@ -62,20 +51,6 @@ public class Fairness
     private static MultiControllerOffOn off_on_app_a = null;
     private static Instance side_b = null;
     private static MultiControllerOffOn off_on_app_b = null;
-
-    
-    // how many ms to wait before requesting b to dump its tasks after a has
-    // started its tasks
-    final static int A_HEADSTART_TIME_MS = 8;
-
-    // These identifiers are associated with each endpoint and appear (in order)
-    // in the threadsafe queue.
-    final static String ENDPOINT_A_IDENTIFIER = "1";
-    final static String ENDPOINT_B_IDENTIFIER = "0";
-
-    // extra debugging flag: something for us to watch out for in case we had an
-    // exception.
-    final public static AtomicBoolean had_exception = new AtomicBoolean(false);
 
     // exact numbers don't matter; they just need to be distinct
     private static final int TCP_SERVICE_REFERENCE_PORT_A = 39318;
@@ -171,11 +146,11 @@ public class Fairness
         ConcurrentLinkedQueue<String> tsafe_queue =
             new ConcurrentLinkedQueue<String>();
 
-        run_operations(
+        FairnessUtil.run_operations(
             off_on_app_a,off_on_app_b,dummy_switch_id,num_external_calls,
             tsafe_queue);
 
-        write_results(result_filename,tsafe_queue);
+        FairnessUtil.write_results(result_filename,tsafe_queue);
         
         // actually tell shims to stop.
         shim_a.stop();
@@ -185,18 +160,6 @@ public class Fairness
         Util.force_shutdown();
     }
 
-
-    public static void write_results(
-        String result_filename,ConcurrentLinkedQueue<String> tsafe_queue)
-    {
-        String to_write = "";
-        for (String endpoint_id : tsafe_queue)
-            to_write += endpoint_id + ",";
-
-        if (had_exception.get())
-            to_write += " **** THERE WAS AN EXCEPTION *** \n";
-        Util.write_results_to_file(result_filename,to_write);
-    }
 
     private static void print_usage()
     {
@@ -216,111 +179,6 @@ public class Fairness
 
         System.out.println(usage_string);
     }
-    
-    /**
-       Dump NUM_EXTERNAL_CALLS operations into system on side_a.  Pause to
-       ensure that side_a registers all of them.  Then dump NUM_EXTERNAL_CALLS
-       onto side_b.
-     */
-    public static void run_operations(
-        IOffOnApplication app_a, IOffOnApplication app_b,String switch_id,
-        int num_external_calls,ConcurrentLinkedQueue<String> tsafe_queue)
-    {
-        EndpointTask task_a =
-            new EndpointTask(
-                app_a,ENDPOINT_A_IDENTIFIER,switch_id,tsafe_queue);
-        EndpointTask task_b =
-            new EndpointTask(
-                app_b,ENDPOINT_B_IDENTIFIER,switch_id,tsafe_queue);
-
-        ExecutorService executor_a = create_executor();
-        ExecutorService executor_b = create_executor();
-        
-        // put a bunch of tasks rooted at A into system.
-        for (int i = 0; i < num_external_calls; ++i)
-            executor_a.execute(task_a);
-
-        // give those tasks a head start to get started before b can start its
-        // tasks
-        try {
-            Thread.sleep(A_HEADSTART_TIME_MS);
-        } catch (InterruptedException _ex) {
-            _ex.printStackTrace();
-            had_exception.set(true);
-            assert(false);
-            return;
-        }
-        
-        // put a bunch of tasks rooted at B into system
-        for (int i = 0; i < num_external_calls; ++i)
-            executor_b.execute(task_b);
-        
-        // join on executor services
-        executor_a.shutdown();
-        executor_b.shutdown();
-        while (!(executor_a.isTerminated() && executor_b.isTerminated()))
-        {
-            try {
-                Thread.sleep(50);
-            } catch (InterruptedException _ex) {
-                _ex.printStackTrace();
-                had_exception.set(true);
-                assert(false);
-                break;
-            }
-        }
-    }
-
-    
-    private static class EndpointTask implements Runnable
-    {
-        private final IOffOnApplication app;
-        private final String principal_id;
-        private final String switch_id;
-        private final ConcurrentLinkedQueue<String> tsafe_queue;
-        
-        public EndpointTask(
-            IOffOnApplication _app, String _principal_id, String _switch_id,
-            ConcurrentLinkedQueue<String> _tsafe_queue)
-        {
-            app = _app;
-            principal_id = _principal_id;
-            switch_id = _switch_id;
-            tsafe_queue = _tsafe_queue;
-        }
-
-        public void run ()
-        {
-            try
-            {
-                app.single_op(switch_id);
-                tsafe_queue.add(principal_id + "|" + System.nanoTime());
-            }
-            catch(Exception ex)
-            {
-                ex.printStackTrace();
-                had_exception.set(true);
-            }
-        }
-    }
-        
-
-    public static ExecutorService create_executor()
-    {
-        ExecutorService executor = Executors.newCachedThreadPool(
-            new ThreadFactory()
-            {
-                // each thread created is a daemon
-                public Thread newThread(Runnable r)
-                {
-                    Thread t=new Thread(r);
-                    t.setDaemon(true);
-                    return t;
-                }
-            });
-        return executor;
-    }
-
     
     /**
        Constructs side_b pronghorn instance in response to a tcp
---
+++ src/experiments/src/FairnessUtil.java
@@ -0,0 +1,154 @@
+package experiments;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ThreadFactory;
+import java.io.IOException;
+
+import experiments.IOffOnApplicationJava.IOffOnApplication;
+
+public class FairnessUtil
+{
+    /**
+       how many ms to wait before requesting b to dump its tasks after
+       a has started its tasks
+    */
+    final static int A_HEADSTART_TIME_MS = 8;
+
+    /**
+       extra debugging flag: something for us to watch out for in case
+       we had an exception.
+    */
+    final public static AtomicBoolean had_exception =
+        new AtomicBoolean(false);
+
+    /**
+       These identifiers are associated with each endpoint and appear
+       (in order) in the threadsafe queue.
+    */
+    final static String PRINCIPAL_A_IDENTIFIER = "1";
+    final static String PRINCIPAL_B_IDENTIFIER = "0";
+
+    
+    /**
+       Dump num_external_calls operations into system using app_a.
+       Pause to ensure that side_a registers all of them.  Then dump
+       num_external_calls into system using app_b.
+     */
+    public static void run_operations(
+        IOffOnApplication app_a, IOffOnApplication app_b,String switch_id,
+        int num_external_calls,ConcurrentLinkedQueue<String> tsafe_queue)
+    {
+        PrincipalTask task_a =
+            new PrincipalTask(
+                app_a,PRINCIPAL_A_IDENTIFIER,switch_id,tsafe_queue);
+        PrincipalTask task_b =
+            new PrincipalTask(
+                app_b,PRINCIPAL_B_IDENTIFIER,switch_id,tsafe_queue);
+
+        ExecutorService executor_a = create_executor();
+        ExecutorService executor_b = create_executor();
+        
+        // put a bunch of tasks rooted at A into system.
+        for (int i = 0; i < num_external_calls; ++i)
+            executor_a.execute(task_a);
+
+        // give those tasks a head start to get started before b can
+        // start its tasks
+        try {
+            Thread.sleep(A_HEADSTART_TIME_MS);
+        } catch (InterruptedException _ex) {
+            _ex.printStackTrace();
+            had_exception.set(true);
+            assert(false);
+            return;
+        }
+        
+        // put a bunch of tasks rooted at B into system
+        for (int i = 0; i < num_external_calls; ++i)
+            executor_b.execute(task_b);
+        
+        // join on executor services
+        executor_a.shutdown();
+        executor_b.shutdown();
+        while (!(executor_a.isTerminated() && executor_b.isTerminated()))
+        {
+            try
+            {
+                Thread.sleep(50);
+            }
+            catch (InterruptedException _ex)
+            {
+                _ex.printStackTrace();
+                had_exception.set(true);
+                assert(false);
+                break;
+            }
+        }
+    }
+
+    public static void write_results(
+        String result_filename,ConcurrentLinkedQueue<String> tsafe_queue)
+    {
+        String to_write = "";
+        for (String endpoint_id : tsafe_queue)
+            to_write += endpoint_id + ",";
+
+        if (had_exception.get())
+            to_write += " **** THERE WAS AN EXCEPTION *** \n";
+        Util.write_results_to_file(result_filename,to_write);
+    }
+
+
+    
+    private static class PrincipalTask implements Runnable
+    {
+        private final IOffOnApplication app;
+        private final String principal_id;
+        private final String switch_id;
+        private final ConcurrentLinkedQueue<String> tsafe_queue;
+        
+        public PrincipalTask(
+            IOffOnApplication _app, String _principal_id, String _switch_id,
+            ConcurrentLinkedQueue<String> _tsafe_queue)
+        {
+            app = _app;
+            principal_id = _principal_id;
+            switch_id = _switch_id;
+            tsafe_queue = _tsafe_queue;
+        }
+
+        public void run ()
+        {
+            try
+            {
+                app.single_op(switch_id);
+                tsafe_queue.add(principal_id + "|" + System.nanoTime());
+            }
+            catch(Exception ex)
+            {
+                ex.printStackTrace();
+                had_exception.set(true);
+            }
+        }
+    }
+        
+
+    private static ExecutorService create_executor()
+    {
+        ExecutorService executor = Executors.newCachedThreadPool(
+            new ThreadFactory()
+            {
+                // each thread created is a daemon
+                public Thread newThread(Runnable r)
+                {
+                    Thread t=new Thread(r);
+                    t.setDaemon(true);
+                    return t;
+                }
+            });
+        return executor;
+    }
+}

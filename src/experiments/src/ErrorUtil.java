71d45b9559b82d2dd6bc567a5c63ad2c659afb39
bmistree <bmistree@gmail.com>
2014-04-20 18:05:36 -0700

    Refactored single controller error test so that can reuse error test code between single and multiple controllers.

--- src/experiments/src/ErrorUtil.java
+++ src/experiments/src/ErrorUtil.java
@@ -1,5 +1,6 @@
 package experiments;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -9,6 +10,8 @@ import ralph.Variables.NonAtomicTextVariable;
 
 import RalphExtended.ExtendedHardwareOverrides;
 import RalphExtended.IHardwareChangeApplier;
+
+import experiments.IErrorApplicationJava.IErrorApplication;
 
 import pronghorn.FTableUpdate;
 import pronghorn.FlowTableToHardware;
@@ -22,6 +25,12 @@ import pronghorn.SwitchDeltaJava.SwitchDelta;
 
 public class ErrorUtil
 {
+    // gives time to settle changes
+    private static final int SLEEP_TIME_BETWEEN_TESTS_MS = 1000;
+    private static final Random rand = new Random();
+    private static final int MAX_NUM_OPS_BEFORE_CHECK = 20;
+
+    
     /**
        extra debugging flag: something for us to watch out for in case
        we had an exception.
@@ -29,7 +38,189 @@ public class ErrorUtil
     final public static AtomicBoolean had_exception =
         new AtomicBoolean(false);
 
+    public static List<Boolean> run_operations(
+        int num_ops_to_run, IErrorApplication error_app,
+        String faulty_switch_id,List<String> switch_id_list)
+    {
+        List<Boolean> results_list = new ArrayList<Boolean>();
+        /*
+          Successively add and remove flow table entries, even in the
+          face of errors.
+         */
+        for (int i = 0; i < num_ops_to_run; ++i)
+        {
+            try
+            {
+                Thread.sleep(SLEEP_TIME_BETWEEN_TESTS_MS);
+            }
+            catch(InterruptedException ex)
+            {
+                ex.printStackTrace();
+                assert(false);
+            }
+            
+            // perform random number of operations
+            int num_ops_to_perform =
+                rand.nextInt(MAX_NUM_OPS_BEFORE_CHECK) + 1;
+            
+            for (int j = 0; j < num_ops_to_perform; ++j)
+            {
+                try
+                {
+                    if ((j % 2) == 0)
+                        error_app.block_traffic_all_switches();
+                    else
+                        error_app.remove_first_entry_all_switches();
+                }
+                catch (Exception ex)
+                {
+                    ex.printStackTrace();
+                    return results_list;
+                }
+            }
 
+            int num_switches = switch_id_list.size();
+            boolean logical_correct =
+                check_logical_state(
+                    switch_id_list,error_app,num_ops_to_perform,
+                    faulty_switch_id);
+            boolean physical_correct =
+                check_physical_state(
+                    num_ops_to_perform,
+                    // subtracting 1 from total number of switches
+                    // because one switch is the simulated faulty
+                    // software switch
+                    num_switches -1);
+
+            
+            results_list.add(logical_correct && physical_correct);
+
+            // clear hardware
+            clear_hardware(num_switches -1);
+
+            // logical clear switches
+            logical_clear_switches(error_app,switch_id_list);
+        }
+        return results_list;
+    }
+
+    /**
+       Translate true-s to 1-s and false-s to 0-s.
+     */
+    public static void write_results(
+        String output_filename, List<Boolean> results_list)
+    {
+        StringBuffer results_buffer = new StringBuffer();
+        for (Boolean trial_result : results_list)
+        {
+            if (trial_result.booleanValue())
+                results_buffer.append("1,");
+            else
+                results_buffer.append("0,");
+        }
+        Util.write_results_to_file(output_filename,results_buffer.toString());
+    }
+    
+    private static boolean check_logical_state(
+        List<String> switch_id_list,IErrorApplication error_app,
+        int num_ops_to_perform, String faulty_switch_to_skip)
+    {
+        // now, check that all the switches have the correct
+        // number of rules on them, flush hardware, and flush
+        // logical rules.
+        boolean expected_number_rules_zero =
+            (num_ops_to_perform %2) == 0;
+        boolean all_expected = true;
+        for (String pronghorn_switch_id : switch_id_list)
+        {
+            // ignore faulty switch id, because we aren't cleaning it
+            // up after it fails.  Here, we're more concerned that the
+            // other switches stay in sync, despite error.
+            if (pronghorn_switch_id.equals(faulty_switch_to_skip))
+                continue;
+            try
+            {
+                int num_flow_table_entries =
+                    error_app.num_flow_table_entries(pronghorn_switch_id).intValue();
+                if (expected_number_rules_zero)
+                {
+                    if (num_flow_table_entries != 0)
+                        all_expected = false;
+                }
+                else
+                {
+                    if (num_flow_table_entries != 1)
+                        all_expected = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.printStackTrace();
+                assert(false);
+            }
+        }
+        return all_expected;
+    }
+    
+    /**
+       Run through all the switches and check that they have the
+       expected number of flow table entries on them.
+
+       @param {int} num_physical_switches --- Should be the total
+       number of switches in the system, minus one (the one that's
+       used to force errors).
+     */
+    private static boolean check_physical_state(
+        int num_ops_performed, int num_physical_switches)
+    {
+        int expected_number_of_entries = 0;
+        if ((num_ops_performed %2) == 1)
+            expected_number_of_entries = 1;
+
+        List<String> ovs_switch_names =
+            Util.produce_ovs_switch_names(num_physical_switches);
+
+        for (String ovs_switch_name : ovs_switch_names)
+        {
+            int flow_tab_entries =
+                Util.ovs_hardware_flow_table_size(ovs_switch_name);
+            if (flow_tab_entries != expected_number_of_entries)
+                return false;
+        }
+        return true;
+    }
+
+    
+    private static void clear_hardware(int num_physical_switches)
+    {
+        List<String> ovs_switch_names =
+            Util.produce_ovs_switch_names(num_physical_switches);
+
+        for (String ovs_switch_name : ovs_switch_names)
+            Util.ovs_clear_flows_hardware (ovs_switch_name);
+    }
+
+
+    
+    private static void logical_clear_switches(
+        IErrorApplication error_app,List<String> switch_list)
+    {
+        for (String switch_id : switch_list)
+        {
+            try
+            {
+                error_app.logical_clear_switch_do_not_flush_clear_to_hardware(switch_id);
+            }
+            catch(Exception ex)
+            {
+                ex.printStackTrace();
+                assert(false);
+            }
+        }
+    }
+
+    
+    
     static _InternalSwitch create_faulty_switch(
         RalphGlobals ralph_globals,String new_switch_id, boolean speculate,
         float error_probability)
--- src/experiments/src/SingleControllerError.java
+++ src/experiments/src/SingleControllerError.java
@@ -29,11 +29,6 @@ public class SingleControllerError
     // wait this long for pronghorn to add all switches
     public static final int SETTLING_TIME_WAIT = 5000;
     public static final boolean SHOULD_SPECULATE = true;
-
-    // gives time to settle changes
-    private static final int SLEEP_TIME_BETWEEN_TESTS_MS = 1000;
-    private static final int MAX_NUM_OPS_BEFORE_CHECK = 20;
-    private static final Random rand = new Random();
     
     public static void main (String[] args) 
     {
@@ -100,9 +95,6 @@ public class SingleControllerError
         
         // // wait for first switch to connect
         Util.wait_on_switches(num_switches_app);
-
-        // Contains trues if test succeeded, false if test failed.
-        List<Boolean> results_list = new ArrayList<Boolean>();
         
         List<String> switch_id_list = null;
         String faulty_switch_id = "some_switch";
@@ -121,137 +113,19 @@ public class SingleControllerError
             ex.printStackTrace();
             return;
         }
-
-        int num_switches = switch_id_list.size();
         
-
-        /*
-          Successively add and remove flow table entries, even in the
-          face of errors.
-         */
-        for (int i = 0; i < num_ops_to_run; ++i)
-        {
-            try
-            {
-                Thread.sleep(SLEEP_TIME_BETWEEN_TESTS_MS);
-            }
-            catch(InterruptedException ex)
-            {
-                ex.printStackTrace();
-                assert(false);
-            }
-            
-            // perform random number of operations
-            int num_ops_to_perform =
-                rand.nextInt(MAX_NUM_OPS_BEFORE_CHECK) + 1;
-            
-            for (int j = 0; j < num_ops_to_perform; ++j)
-            {
-                try
-                {
-                    if ((j % 2) == 0)
-                        error_app.block_traffic_all_switches();
-                    else
-                        error_app.remove_first_entry_all_switches();
-                }
-                catch (Exception ex)
-                {
-                    ex.printStackTrace();
-                    return;
-                }
-            }
-
-            boolean logical_correct =
-                check_logical_state(
-                    switch_id_list,error_app,num_ops_to_perform,
-                    faulty_switch_id);
-            boolean physical_correct =
-                check_physical_state(
-                    num_ops_to_perform,
-                    // subtracting 1 from total number of switches
-                    // because one switch is the simulated faulty
-                    // software switch
-                    num_switches -1);
-
-            
-            results_list.add(logical_correct && physical_correct);
-
-            // clear hardware
-            clear_hardware(num_switches -1);
-
-            // logical clear switches
-            logical_clear_switches(error_app,switch_id_list);
-        }
-
-        StringBuffer results_buffer = new StringBuffer();
-        for (Boolean trial_result : results_list)
-        {
-            if (trial_result.booleanValue())
-                results_buffer.append("1,");
-            else
-                results_buffer.append("0,");
-        }
-        Util.write_results_to_file(output_filename,results_buffer.toString());
+        // Contains trues if test succeeded, false if test failed.
+        List<Boolean> results_list =
+            ErrorUtil.run_operations(
+                num_ops_to_run,error_app,faulty_switch_id,switch_id_list);
+        ErrorUtil.write_results(output_filename,results_list);
 
         // stop and forst stop
         shim.stop();
         Util.force_shutdown();
     }
 
-    
-    private static void logical_clear_switches(
-        ErrorApplication error_app,List<String> switch_list)
-    {
-        for (String switch_id : switch_list)
-        {
-            try
-            {
-                error_app.logical_clear_switch_do_not_flush_clear_to_hardware(switch_id);
-            }
-            catch(Exception ex)
-            {
-                ex.printStackTrace();
-                assert(false);
-            }
-        }
-    }
-    
-    private static void clear_hardware(int num_physical_switches)
-    {
-        List<String> ovs_switch_names =
-            Util.produce_ovs_switch_names(num_physical_switches);
-
-        for (String ovs_switch_name : ovs_switch_names)
-            Util.ovs_clear_flows_hardware (ovs_switch_name);
-    }
-
-    /**
-       Run through all the switches and check that they have the
-       expected number of flow table entries on them.
-
-       @param {int} num_physical_switches --- Should be the total
-       number of switches in the system, minus one (the one that's
-       used to force errors).
-     */
-    private static boolean check_physical_state(
-        int num_ops_performed, int num_physical_switches)
-    {
-        int expected_number_of_entries = 0;
-        if ((num_ops_performed %2) == 1)
-            expected_number_of_entries = 1;
-
-        List<String> ovs_switch_names =
-            Util.produce_ovs_switch_names(num_physical_switches);
-
-        for (String ovs_switch_name : ovs_switch_names)
-        {
-            int flow_tab_entries =
-                Util.ovs_hardware_flow_table_size(ovs_switch_name);
-            if (flow_tab_entries != expected_number_of_entries)
-                return false;
-        }
-        return true;
-    }
+        
     
     private static void print_usage()
     {
@@ -271,44 +145,4 @@ public class SingleControllerError
         System.out.println(usage_string);
     }
 
-    private static boolean check_logical_state(
-        List<String> switch_id_list,ErrorApplication error_app,
-        int num_ops_to_perform, String faulty_switch_to_skip)
-    {
-        // now, check that all the switches have the correct
-        // number of rules on them, flush hardware, and flush
-        // logical rules.
-        boolean expected_number_rules_zero =
-            (num_ops_to_perform %2) == 0;
-        boolean all_expected = true;
-        for (String pronghorn_switch_id : switch_id_list)
-        {
-            // ignore faulty switch id, because we aren't cleaning it
-            // up after it fails.  Here, we're more concerned that the
-            // other switches stay in sync, despite error.
-            if (pronghorn_switch_id.equals(faulty_switch_to_skip))
-                continue;
-            try
-            {
-                int num_flow_table_entries =
-                    error_app.num_flow_table_entries(pronghorn_switch_id).intValue();
-                if (expected_number_rules_zero)
-                {
-                    if (num_flow_table_entries != 0)
-                        all_expected = false;
-                }
-                else
-                {
-                    if (num_flow_table_entries != 1)
-                        all_expected = false;
-                }
-            }
-            catch (Exception ex)
-            {
-                ex.printStackTrace();
-                assert(false);
-            }
-        }
-        return all_expected;
-    }
 }

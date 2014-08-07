904b2ea38152221da8d3156bc8169ec24d3d150c
bmistree <bmistree@gmail.com>
2014-08-06 18:26:03 -0700

    Crafting rough flow mods from user-submitted ralph flow table updates.  Added a couple of fixmes to code and registered issues #18 and #19 to allow all openflow 1.3 instructions.

--- src/pronghorn/src/ft_ops/FTableUpdate.java
+++ src/pronghorn/src/ft_ops/FTableUpdate.java
@@ -1,6 +1,21 @@
 package pronghorn.ft_ops;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.openflow.protocol.OFFlowMod;
+import org.openflow.protocol.OFMatch;
+import org.openflow.protocol.action.OFAction;
+import org.openflow.protocol.instruction.OFInstruction;
+import org.openflow.protocol.instruction.OFInstructionActions;
+import org.openflow.protocol.instruction.OFInstructionWriteActions;
+
+import net.floodlightcontroller.packet.IPv4;
+
+
 
 public class FTableUpdate
 {
@@ -12,6 +27,10 @@ public class FTableUpdate
        deletion.
      */
     private boolean insertion;
+
+
+    protected static final Logger log =
+        LoggerFactory.getLogger(FTableUpdate.class);
     
     public static FTableUpdate create_insert_update(
         String src_ip, String dst_ip, String actions)
@@ -43,12 +62,59 @@ public class FTableUpdate
      */
     public OFFlowMod to_flow_mod(int xid)
     {
-        /**
-           TODO: fill in stub method
-         */
-        assert(false);
-        return null;
+        OFFlowMod to_return = new OFFlowMod();
+
+        // decide whether to add or delete
+        if (insertion)
+            to_return.setCommand(OFFlowMod.OFPFC_ADD);
+        else
+            to_return.setCommand(OFFlowMod.OFPFC_DELETE_STRICT);
+
+        
+        // handle matches
+        OFMatch match = new OFMatch();
+        if (src_ip != null)
+            match.setNetworkSource(IPv4.toIPv4Address(src_ip));
+        if (dst_ip != null)
+            match.setNetworkDestination(IPv4.toIPv4Address(dst_ip));
+        to_return.setMatch(match);
+        
+        // add operations for insertions
+        if (insertion)
+        {
+            // FIXME: currently, solely allowing instructions for
+            // actions, not for applying actions or jumping to
+            // different tables, etc.
+            List<OFAction> actions_list = string_action_to_actions_list();
+            OFInstructionActions instruction_actions =
+                new OFInstructionWriteActions(actions_list);
+            instruction_actions.setActions(actions_list);
+
+            List<OFInstruction> instructions = new ArrayList<OFInstruction>();
+            to_return.setInstructions(instructions);
+        }
+        
+        return to_return;
     }
+
+
+    /**
+       Takes actions string and returns a list of OFActions
+       corresponding to those.
+     */
+    private List<OFAction> string_action_to_actions_list()
+    {
+        // FIXME: Currently, returning empty action.  This is because
+        // I should ultimately write different types of actions in
+        // ralph instead of just using a string.
+        log.error(
+            "Using empty set of actions in FTableUpdate.  " +
+            "See FIXME near logging.");
+        
+        List<OFAction> to_return = new ArrayList<OFAction>();
+        return to_return;
+    }
+    
     
     /**
        Takes current flow mod instruction and creates an FTableUpdate
